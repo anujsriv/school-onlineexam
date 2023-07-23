4f4b5bcc781cb5fc042943cd95229f37a5dc218d
dcha8899 <140246502+dcha8899@users.noreply.github.com>
2023-07-23 14:29:31 +0530

    Update UserAlreadyLoggedOutException.java

--- backend/src/main/java/com/asan/osms/exception/UserAlreadyLoggedOutException.java
+++ backend/src/main/java/com/asan/osms/exception/UserAlreadyLoggedOutException.java
@@ -5,7 +5,7 @@ public class UserAlreadyLoggedOutException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 
 	public UserAlreadyLoggedOutException(String name) {
-		super(String.format("User %s already logged out..", name));
+		super(String.format("User %s already logged out...", name));
 	}
 
 }
